f71cdaf646942d30bd8009986263e467d14440f2
Lucas Owino <lucowish35@gmail.com>
2020-05-26 02:42:41 +0300

    updated proper format of web: gunicorn manage:app in procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn manage:app
+web: gunicorn pic_galore.wsgi --log-file -
--- pic_galore/settings.py
+++ pic_galore/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pic_galore.urls'
@@ -121,11 +122,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #These settings allow us to to define a special route that will display our uploaded images
 MEDIA_URL = '/media/'
