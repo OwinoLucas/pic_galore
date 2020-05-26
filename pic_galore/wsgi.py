16e315a9de43b368e798d43a4a949987cf03a003
Lucas Owino <lucowish35@gmail.com>
2020-05-26 11:04:14 +0300

    edit wsgi.py file due to deployment failure

--- pic_galore/wsgi.py
+++ pic_galore/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pic_galore.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pic_galore.settings')
 
 application = get_wsgi_application()
